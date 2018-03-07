7614d0fc2b82a99a425d547335a7d4ffc2e45a46
shifeng <shifeng199307@gmail.com>
2018-03-07 19:54:29 +0800

    新增setOption示例

--- example/App.js
+++ example/App.js
@@ -3,7 +3,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 import Echarts from './src/index.js';
 
@@ -27,9 +28,29 @@ export default class App extends Component {
     };
     return (
       <View style={styles.container}>
-        <Echarts option={option}/>
+        <Echarts ref="echarts1" option={option}/>
+        <TouchableOpacity onPress={this.editOption} ><Text>点我改变echarts option</Text></TouchableOpacity>
       </View>
     );
+  }
+
+  editOption = () => {
+    const newOption = {
+      xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [{
+          data: [123, 321, 123, 321, 1290, 111, 654],
+          type: 'line',
+          areaStyle: {}
+      }]
+    };
+    this.refs.echarts1.setOption(newOption)
   }
 }
 const styles = StyleSheet.create({
