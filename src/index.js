6220971fb956fb5763e9498819816f998e4aacfc
Jia Song <593908937@qq.com>
2018-04-12 14:24:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet,Platform} from 'react-native';
+import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 import PropTypes from 'prop-types';
@@ -9,23 +9,23 @@ export default class Echarts extends Component {
     super(props);
   }
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
+    if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
   }
   static defaultProps = {
     backgroundColor: '#00000000'
-  } 
+  }
   render() {
-    const source = (Platform.OS == 'ios') ? require('./index.html') : {'uri':'file:///android_asset/echarts/index.html'} // 修复android release路径问题
+    const source = (Platform.OS == 'ios') ? require('./index.html') : { 'uri': 'file:///android_asset/echarts/index.html' } // 修复android release路径问题
     return (
-      <View style={{flex: 1, height: this.props.height || 400}}>
+      <View style={{ height: this.props.height || 400, }}>
         <WebView
           ref="chart"
-          renderLoading={this.props.renderLoading || (()=><View style={{backgroundColor:this.props.backgroundColor}}/>)} // 设置空View，修复ioswebview闪白
-          style={{backgroundColor:this.props.backgroundColor}} // 设置背景色透明，修复android闪白
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
+          renderLoading={this.props.renderLoading || (() => <View style={{ backgroundColor: this.props.backgroundColor }} />)} // 设置空View，修复ioswebview闪白
+          style={{ backgroundColor: this.props.backgroundColor, }} // 设置背景色透明，修复android闪白
+          scrollEnabled={false}
+          injectedJavaScript={renderChart(this.props)}
           source={source}
         />
       </View>
@@ -37,7 +37,7 @@ export default class Echarts extends Component {
 }
 
 Echarts.propTypes = {
-  option:PropTypes.object,
+  option: PropTypes.object,
   backgroundColor: PropTypes.string,
   width: PropTypes.number,
   height: PropTypes.number,
