5e5146cf39a4b1e4a7511f263e4a9b9335d5284c
Jia Song <593908937@qq.com>
2018-04-19 20:15:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import echarts from './echarts.min';
 import PropTypes from 'prop-types';
 
 export default class Echarts extends Component {
+  static propTypes = {
+    option: PropTypes.object,
+    backgroundColor: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    renderLoading: PropTypes.func,
+    onPress: PropTypes.func
+  }
+  static defaultProps = {
+    backgroundColor: '#00000000'
+  }
   constructor(props) {
     super(props);
   }
@@ -13,8 +24,14 @@ export default class Echarts extends Component {
       this.refs.chart.reload();
     }
   }
-  static defaultProps = {
-    backgroundColor: '#00000000'
+  _onPress = () => {
+    this.props.onPress && this.props.onPress(JSON.parse(event.nativeEvent.data))
+  }
+  _renderLoading = () => {
+    if (this.props.renderLoading) {
+      return this.props.renderLoading
+    }
+    return <View style={{ backgroundColor: this.props.backgroundColor }} />
   }
   render() {
     const source = (Platform.OS == 'ios') ? require('./index.html') : { 'uri': 'file:///android_asset/echarts/index.html' } // 修复android release路径问题
@@ -22,11 +39,12 @@ export default class Echarts extends Component {
       <View style={{ height: this.props.height || 400, }}>
         <WebView
           ref="chart"
-          renderLoading={this.props.renderLoading || (() => <View style={{ backgroundColor: this.props.backgroundColor }} />)} // 设置空View，修复ioswebview闪白
+          renderLoading={this._renderLoading} // 设置空View，修复ioswebview闪白
           style={{ backgroundColor: this.props.backgroundColor, }} // 设置背景色透明，修复android闪白
           scrollEnabled={false}
           injectedJavaScript={renderChart(this.props)}
           source={source}
+          onMessage={this._onPress}
         />
       </View>
     );
@@ -36,10 +54,4 @@ export default class Echarts extends Component {
   }
 }
 
-Echarts.propTypes = {
-  option: PropTypes.object,
-  backgroundColor: PropTypes.string,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  renderLoading: PropTypes.func
-}
+
