09f569d089df2dfbe89e0c3285e9b2e692bfafb8
shifeng <shifeng199307@gmail.com>
2018-11-30 11:38:12 +0800

    修复1.5.2版本出现的不能渲染的bug，使用最新版本rn重写示例

--- example/index.js
+++ example/index.js
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
+/** @format */
 
-AppRegistry.registerComponent('example', () => App);
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
--- example/src/router/index.js
+++ example/src/router/index.js
@@ -1,30 +1,48 @@
 import React, {
   Component
 } from 'react';
-import {
-  createStackNavigator
-} from 'react-navigation';
-
+import {createAppContainer, createStackNavigator, StackViewTransitionConfigs} from 'react-navigation';
 // 引入页面容器
 import {
   IndexPage,
   MapPage
 } from '../pages';
 
+
+const IOS_MODAL_ROUTES = ['Login'];
+
+const dynamicModalTransition = (transitionProps, prevTransitionProps) => {
+  const isModal = IOS_MODAL_ROUTES.some(
+    screenName =>
+      screenName === transitionProps.scene.route.routeName ||
+      (prevTransitionProps && screenName === prevTransitionProps.scene.route.routeName)
+  )
+  return StackViewTransitionConfigs.defaultTransitionConfig(
+    transitionProps,
+    prevTransitionProps,
+    isModal
+  );
+};
+
 /* ****************************** 总导航 ****************************** */
-const AppNavigator = createStackNavigator({
-  Main: {
-    screen: IndexPage,
-    navigationOptions: {
-      header: null,
-      gesturesEnabled: false // 左侧滑动返回   ios默认开启，android默认关闭
+const AppNavigator = createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: IndexPage,
+        navigationOptions: {
+          header: null,
+          gesturesEnabled: false // 左侧滑动返回   ios默认开启，android默认关闭
+        }
+      },
+      Map: {
+        screen: MapPage,
+      }
+    }, {
+      initialRouteName: 'Main',
+      transitionConfig: dynamicModalTransition
     }
-  },
-  Map: {
-    screen: MapPage,
-  }
-}, {
-  initialRouteName: 'Main'
-});
+  )
+);
 
 export default AppNavigator;
